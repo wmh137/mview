b42bba4b993bf1c5d9134d6fa0d550c4d56e78dd
wmh137 <wmh137@mail.ustc.edu.cn>
2022-12-01 21:40:31 +0800

    grid on and add xps to xytext

--- py-script/xytext.py
+++ py-script/xytext.py
@@ -12,22 +12,29 @@ for i in range(3, pathNum):
     for j in range(skipLine):
         rFile.readline()
     inLine = rFile.readline()
-    while(inLine):
-        inXY = inLine.split()
-        X.append(float(inXY[0]))
-        Y.append(float(inXY[1]))
+    inXY = inLine.replace(",", " ").split()
+    while inLine:
+        if len(inXY)>1:
+            X.append(float(inXY[0]))
+            Y.append(float(inXY[1]))
         inLine = rFile.readline()
+        inXY = inLine.replace(",", " ").split()
     rFile.close()
     plt.plot(X, Y, label = path)
 if(labelType == "raman"):
     plt.xlabel("Raman Shift (cm$^{-1}$)")
     plt.ylabel("Intensity (a.u.)")
 elif(labelType == "xrd"):
-    plt.xlabel("$\\theta\\ (\degree)$")
+    plt.xlabel("$2\\theta\\ (\degree)$")
     plt.ylabel("Intensity (a.u.)")
+elif(labelType == "xps"):
+    plt.xlabel("Binding Energy (eV)")
+    plt.ylabel("Intensity (a.u.)")
+    plt.gca().invert_xaxis()
 elif(labelType == "xy"):
     plt.xlabel("x")
     plt.ylabel("y")
 plt.title(labelType)
 plt.legend()
+plt.grid()
 plt.show()
